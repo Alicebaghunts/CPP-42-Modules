491cb692f60d6aca473c6e22a9aeb77070870086
Alice <alisa.bagunts@gmail.com>
2026-05-07 22:59:18 +0400

    a

--- CPP-module03/ex02/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "FragTrap.hpp"
-
-int main()
-{
-	FragTrap f("Charlie");
-	f.highFivesGuys();
-	f.attack("Target");
-	return 0;
-}
--- CPP-module03/ex03/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "DiamondTrap.hpp"
-
-int main()
-{
-	DiamondTrap d("Diana");
-	d.whoAmI();
-	d.attack("Target");
-	return 0;
-}
