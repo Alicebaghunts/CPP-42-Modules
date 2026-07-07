d89bb352d5705848bf3db82a8d3f16734c4248fb
Alice <alisa.bagunts@gmail.com>
2026-07-07 16:39:47 +0400

    adding first ex

--- CPP-module05/ex00/main.cpp
+++ CPP-module05/ex00/main.cpp
@@ -26,25 +26,25 @@ int	main(void)
 	std::cout << SOFT_PURPLE << "\n------ Testing exceptions ------\n" << RESET;
 
 	try {
-		Bureaucrat tooHigh("TooHigh", 0);
+		Bureaucrat tooHigh("MxelaShat", 0);
 	}
 	catch (std::exception &e) {
-		std::cout << "Test 1: " << e.what() << std::endl;
+		std::cout << "Test 1 result: " << e.what() << std::endl;
 	}
 
 	try {
-		Bureaucrat tooLow("TooLow", 151);
+		Bureaucrat tooLow("ShatXreg", 151);
 	}
 	catch (std::exception &e) {
-		std::cout << "Test 2: " << e.what() << std::endl;
+		std::cout << "Test 2 result: " << e.what() << std::endl;
 	}
 
 	try {
-		Bureaucrat b("Test", 1);
+		Bureaucrat b("Sxaly", 1);
 		b.incrementGrade(); 
 	}
 	catch (std::exception &e) {
-		std::cout << "Test 3: " << e.what() << std::endl << std::endl;
+		std::cout << "Test 3 result: " << e.what() << std::endl << std::endl;
 	}
 
 	return (0);
