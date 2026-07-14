fc8efc1f7256439b4c908e6dde8f397b52fad4ee
Alice <alisa.bagunts@gmail.com>
2026-07-14 16:56:23 +0400

    fix something

--- CPP-module05/ex03/Intern.cpp
+++ CPP-module05/ex03/Intern.cpp
@@ -5,54 +5,50 @@
 
 typedef AForm *(*FormCreator)(const std::string &target);
 
-static AForm *createShrubbery(const std::string &target)
-{
+static AForm *createShrubbery(const std::string &target) {
     return new ShrubberyCreationForm(target);
 }
 
-static AForm *createRobotomy(const std::string &target)
-{
+static AForm *createRobotomy(const std::string &target) {
     return new RobotomyRequestForm(target);
 }
 
-static AForm *createPardon(const std::string &target)
-{
+static AForm *createPardon(const std::string &target) {
     return new PresidentialPardonForm(target);
 }
 
-Intern::Intern() {}
-Intern::Intern(const Intern &) {}
-Intern &Intern::operator=(const Intern &)
-{
+Intern::Intern() { }
+
+Intern::Intern(const Intern &) { }
+
+Intern &Intern::operator=(const Intern &) {
     return *this;
 }
 
-Intern::~Intern() {}
+Intern::~Intern() { }
 
-AForm *Intern::makeForm(const std::string &formName, const std::string &target) const
-{
-    struct FormEntry
-    {
+AForm *Intern::makeForm(const std::string &formName, const std::string &target) const {
+    struct FormEntry {
         const char *name;
         FormCreator creator;
     };
 
     static const FormEntry table[] = {
-        { "shrubbery creation", createShrubbery },
-        { "robotomy request", createRobotomy },
-        { "presidential pardon", createPardon }
+		{ "shrubbery creation", createShrubbery },
+		{ "robotomy request", createRobotomy },
+		{ "presidential pardon", createPardon }
     };
 
     for (int i = 0; i < 3; ++i)
     {
-        if (formName == table[i].name)
-        {
-            AForm *form = table[i].creator(target);
-            std::cout << "Intern creates " << table[i].name << std::endl;
-            return form;
-        }
+		if (formName == table[i].name)
+		{
+			AForm *form = table[i].creator(target);
+			
+			std::cout << "Intern creates " << table[i].name << std::endl;
+			return form;
+		}
     }
-
-    std::cerr << "Error: unknown form '" << formName << "'" << std::endl;
+    std::cerr << "Error: unknown form ('" << formName << "'" << std::endl;
     return 0;
 }
